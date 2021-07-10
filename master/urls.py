a8a0cfd5a7c779fa49ef7a84999d5ad1a306ab85
BehradMazaheri <BehradMazaheriOffice@gmail.com>
2021-07-10 15:26:08 +0430

    add exercise and answer url

--- master/urls.py
+++ master/urls.py
@@ -4,6 +4,9 @@ from master import views as ms
 urlpatterns = [
     path('courses/', ms.CourseListCreate.as_view()),
     path('courses/<int:pk>', ms.CourseRUD.as_view()),
-    path('courses/<int:pk>/subjects', ms.SubjectListCreate.as_view())
+    path('courses/<int:pk>/subjects', ms.SubjectListCreate.as_view()),
 
+    path('exercises/', ms.ExerciseListCreate.as_view()),
+    path('exercises/<int:pk>', ms.ExerciseRUD.as_view()),
+    path('exercises/<int:pk>/answers', ms.ExerciseAnswerListCreate.as_view())
 ]
