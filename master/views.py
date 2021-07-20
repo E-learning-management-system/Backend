cac68d1596b2f2b755e98cea5fcc5ab1b0d663f9
BehradMazaheri <BehradMazaheriOffice@gmail.com>
2021-07-20 13:52:34 +0430

    create student exercise api

--- master/views.py
+++ master/views.py
@@ -495,5 +495,5 @@ class StudentExerciseList(generics.ListAPIView):
     http_method_names = ['get']
 
     def get_queryset(self):
-        courses = CourseStudent.objects.filter(user=self.request.user).course
+        courses = CourseStudent.objects.get(user=self.request.user).course
         return Exercise.objects.filter(Q(course__in=[courses]))
