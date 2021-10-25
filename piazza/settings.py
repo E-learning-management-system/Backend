361c645dd4ae2ab4efd1d32a21acfc913f25cc1c
AmirVahdati <amirvahdati16@gmail.com>
2021-10-25 11:07:22 +0330

    data base

--- piazza/settings.py
+++ piazza/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
 
     'default': {
         'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.mysql'),
-        'NAME': os.environ.get('SQL_DATABASE', 'soren_db'),
-        'USER': os.environ.get('SQL_USER', 'soren'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', 'soren_1234'),
+        'NAME': os.environ.get('SQL_DATABASE', 'piazza_db'),
+        'USER': os.environ.get('SQL_USER', 'piazza'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'piazza_1234'),
         'HOST': os.environ.get('SQL_HOST', 'localhost'),
     }
 }
