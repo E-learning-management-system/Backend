acba8fc35b078ead1e487fa836a853cb38e9eb81
Behrad_Mazaheri <BehradMazaheriOffice@gmail.com>
2022-01-31 11:07:52 +0330

    debug

--- master/views.py
+++ master/views.py
@@ -598,7 +598,7 @@ class AnswerCreate(generics.CreateAPIView):
     def perform_create(self, serializer):
         exercise = Exercise.objects.get(pk=self.kwargs['pk'])
         exerciseCourseStudent = CourseStudent.objects.filter(course=exercise.course)
-        if self.request.user in exerciseCourseStudent:
+        if self.request.user in exerciseCourseStudent[:]['user']:
             serializer.save(exercise=exercise, user=self.request.user)
         else:
             raise ValidationError('شما به این عمل دسترسی ندارید')
