3a1a85cf8b3a609349414352b36ccd32c9e57883
AmirVahdati <amirvahdati16@gmail.com>
2022-05-14 10:58:54 +0330

    debug

--- master/tests.py
+++ master/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+import os
+import pytest
+import json
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'piazza.settings')
+import django
+
+django.setup()
+client = Client()
+
+class TestBase(TestCase):
+    def test_zero_returns_nothing(self):
+        client.login(email='amir@gmail.com', password='abcd')
+        response = client.get('/soren/courses/')
+        self.assertEqual(json.loads(response.content).get('results'), [])
+
+
+
+
