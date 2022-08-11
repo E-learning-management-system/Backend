698ed265f56c753ec9e46fce1f00e5de08ee5045
AmirVahdati <amirvahdati16@gmail.com>
2022-08-11 11:49:28 +0330

    comment

--- master/views.py
+++ master/views.py
@@ -210,9 +210,9 @@ class LargeResultsSetPagination(PageNumberPagination):
 
 
 class CommentSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 4
     page_query_param = 'page_size'
-    max_page_size = 2
+    max_page_size = 4
 
 
 class CourseList(generics.ListAPIView):
